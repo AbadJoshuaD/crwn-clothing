245a964b73fd7a45dc14ee04390366e944ca04b7
Abad Joshua <abad.joshuad@gmail.com>
2019-10-05 23:01:04 +0800

    Done on adding react router dom to the application and made an initial dynamic route to the app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from "react";
 import "./App.css";
+import { Switch, Route } from 'react-router-dom'
 import { HomePage } from "./pages/homepage.component";
+
+const HatsPage = () => (
+  <div>HATS PAGE</div>
+)
 function App() {
   return (
+
     <div className="App">
-      <HomePage></HomePage>
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/hats' component={HatsPage} />
+      </Switch>
     </div>
   );
 }
