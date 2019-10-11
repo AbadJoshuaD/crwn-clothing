d6d26ccf260e4e346b7b639cd9a3642aa8e6ba29
Abad Joshua <abad.joshuad@gmail.com>
2019-10-11 20:39:41 +0800

    some changes and added server

--- client/src/components/stripe-button/strip-button.component.jsx
+++ client/src/components/stripe-button/strip-button.component.jsx
@@ -1,13 +1,30 @@
 import React from 'react'
 import StripeCheckOut from 'react-stripe-checkout'
+import axios from 'axios'
 //A simple stripe integration to the application that allow us to access the stripe button and process payment later on
 const StripeCheckOutButton = ({price}) => {
     const priceForStripe = price *100;
     const publishableKey = 'pk_test_ITcfbEsA7SzTvvkRTEdog9KP006HE0dTUj'
+    
     const onToken = token => {
-        console.log(token);
-        alert('Payment Successful');
-    }
+        axios({
+          url: 'payment',
+          method: 'post',
+          data: {
+            amount: priceForStripe,
+            token: token
+          }
+        })
+          .then(response => {
+            alert('Succesful payment');
+          })
+          .catch(error => {
+            console.log('Payment Error: ',JSON.parse(error));
+            alert(
+              'There was an issue with your payment! Please make sure you use the provided credit card.'
+            );
+          });
+      };
     return(
         <StripeCheckOut
         label='Pay Now'
--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,7 +1,6 @@
 import { createStore, applyMiddleware } from 'redux';
 import { persistStore } from 'redux-persist';
 import rootReducer from './root-reducer';
-import thunk from 'redux-thunk'
 import logger from 'redux-logger'
 import createSagaMiddleware from 'redux-saga'
 import rootSaga from './root-sagas'
@@ -9,11 +8,11 @@ import rootSaga from './root-sagas'
 //We are now using redux thunk to check if 
 const sagaMiddleware = createSagaMiddleware();
 const middlewares = [logger, sagaMiddleware];
+if (process.env.NODE_ENV === 'development') {
+    middlewares.push(logger);
+}
 
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
-
 sagaMiddleware.run(rootSaga);
-
 export const persistor = persistStore(store);
-
 export default { store, persistStore }
