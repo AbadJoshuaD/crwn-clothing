e7b57d95781c94632f6ae85612a0d124c558cb67
Abad Joshua <abad.joshuad@gmail.com>
2019-10-11 10:27:16 +0800

    convert some async function to redux sage generator function

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,11 +2,16 @@ import { createStore, applyMiddleware } from 'redux';
 import { persistStore } from 'redux-persist';
 import rootReducer from './root-reducer';
 import thunk from 'redux-thunk'
+import createSagaMiddleware from 'redux-saga'
+import { fetchCollectionStart } from './shop/shop.sagas'
 
 //We are now using redux thunk to check if 
-const middlewares = [thunk];
+const sagaMiddleware = createSagaMiddleware();
+const middlewares = [sagaMiddleware];
 
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
+
+sagaMiddleware.run(fetchCollectionStart);
 
 export const persistor = persistStore(store);
 
