17df54a653846764899eda0ae7d3a4b672ff0918
Abad Joshua <abad.joshuad@gmail.com>
2019-10-11 17:08:08 +0800

    updated some of the files that use lifecycle methods and use hooks

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,24 +1,18 @@
-import React from 'react';
+import React,{useEffect} from 'react';
 import { Route } from 'react-router-dom';
 import { connect } from 'react-redux';
 import {fetchCollectionsStart } from '../../redux/shop/shop.actions';
 import CollectionsOverviewContainer from '../../components/collections-overview/collections-overview.container'
 import CollectionPageContainer from '../collection/collection.container'
 
-
-
-
 // Created a shop page class component since the data is coming from firebase we need to store the state of the loading(Spinner);
 // If the promise of the firebase data was already been loaded.
-class ShopPage extends React.Component {
+const ShopPage =({fetchCollectionsStart,match})=> {
 
-  componentDidMount() {
-   const { fetchCollectionsStart } = this.props;
-   fetchCollectionsStart();
-  }
+    useEffect(()=>{
+    fetchCollectionsStart();
+    },[fetchCollectionsStart])
 
-  render() {
-    const { match } = this.props;
     return (
       <div className='shop-page'>
         <Route
@@ -33,7 +27,7 @@ class ShopPage extends React.Component {
       </div>
     );
   }
-}
+
 
 
 const mapDispatchToProps = dispatch => ({
