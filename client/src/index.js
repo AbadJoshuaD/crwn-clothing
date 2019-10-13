4229af718432b8e57c52efca94ffb46771101932
Abad Joshua <abad.joshuad@gmail.com>
2019-10-13 22:59:38 +0800

    Make the application PWA compliant

--- client/src/index.js
+++ client/src/index.js
@@ -6,6 +6,7 @@ import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux';
 import { store, persistor } from './redux/store';
 import { PersistGate } from 'redux-persist/integration/react'
+import * as serviceWorker from './serviceWorker'
 
 
 ReactDOM.render(
@@ -19,6 +20,7 @@ ReactDOM.render(
     </Provider>
     , document.getElementById("root"));
 
+serviceWorker.register()
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
